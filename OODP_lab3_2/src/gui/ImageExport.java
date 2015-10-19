79f63c12f4e509886278df9a2ad0dd0e16ad0361
MiranBatti <miran.batti@gmail.com>
2015-10-19 19:38:26 +0200

    Projekt

--- OODP_lab3_2/src/gui/ImageExport.java
+++ OODP_lab3_2/src/gui/ImageExport.java
@@ -1,32 +1,23 @@
 package gui;
 
-import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-
 public class ImageExport {
-	BufferedImage bi;
-	Graphics g;
-	DrawingPanel dp;
 	
 	public ImageExport(DrawingPanel dp) {
-		this.dp = dp;
-		bi = new BufferedImage(this.dp.getWidth(), this.dp.getHeight(), BufferedImage.TYPE_INT_RGB);
-		this.dp.paint(this.dp.getGraphics2D());
-		g = bi.getGraphics();
-		g = bi.createGraphics();
+		BufferedImage bi = new BufferedImage(dp.getWidth(), dp.getHeight(), BufferedImage.TYPE_INT_RGB);;
+		Graphics2D g = bi.createGraphics();
+		dp.paint(g);
+		g.dispose();
+	}
+	
+	public ImageExport() {
+		
 	}
 	
 	public void exportJPEG() throws IOException {
-		try {
-			bi = ImageIO.read(new File("figures.jpg"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		
-		ImageIO.write(bi, "jpeg", new File("figures.jpg"));
+		
 	}
 }
